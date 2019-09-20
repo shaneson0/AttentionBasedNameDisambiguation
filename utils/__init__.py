591801e810c789182ce8824eb2f5783726b3bddb
shanxuan <781244184@qq.com>
2019-09-20 12:22:23 +0800

    调参

--- utils/__init__.py
+++ utils/__init__.py
@@ -41,7 +41,7 @@ def getSetting():
     flags.DEFINE_integer('is_sparse', 0, 'Whether input features are sparse.')
 
     flags.DEFINE_float('SoftmaxVariable', 0.3, 'Weight for softmax.')
-    flags.DEFINE_float('KLlossVariable', 0.01, 'Weight for KL loss on graph comparing.')
+    flags.DEFINE_float('KLlossVariable', 0.001, 'Weight for KL loss on graph comparing.')
     flags.DEFINE_float('CenterLossVariable', 1, 'Weight for the cluster loss --- CenterLoss .')
     flags.DEFINE_float('ReconstructVariable', 1, 'Weight for the cluster loss --- CenterLoss .')
     flags.DEFINE_float('L2Scale', 0, 'Weight for L2 regular')
