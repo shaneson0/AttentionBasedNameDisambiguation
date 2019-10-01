e33b89e868a895f26ff3d964f9b30050ade420f3
shanxuan <781244184@qq.com>
2019-10-01 19:41:48 +0800

    softmax loss 计算错误

--- model/model_optimizer.py
+++ model/model_optimizer.py
@@ -153,7 +153,7 @@ class OptimizerDualGCNAutoEncoder(object):
 
     def kl_divergence(self, p, q):
         # y = p / q
-        return tf.reduce_sum(tf.multiply(p, tf.log(p) - tf.log(q)))
+        return tf.reduce_sum(tf.multiply(p, tf.SpecialLog(p) - tf.SpecialLog(q)))
         # return tf.log(p)
         # return self.KLloss(p,q)
         # return tf.abs(tf.reduce_sum(p * (self.SpecialLog(p) - self.SpecialLog(q))))
