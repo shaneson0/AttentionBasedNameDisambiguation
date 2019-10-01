0ab72625c9dea279ac854e412effaad12a98d98a
shanxuan <781244184@qq.com>
2019-10-01 19:02:15 +0800

    更换kl散度的计算方式

--- model/model_optimizer.py
+++ model/model_optimizer.py
@@ -153,9 +153,11 @@ class OptimizerDualGCNAutoEncoder(object):
         return tf.log(tf.clip_by_value(y,1e-8,1.0))
 
     def kl_divergence(self, p, q):
+        KL = tf.reduce_mean(-tf.nn.softmax_cross_entropy_with_logits(p, q))
+        return KL
         # return tf.log(p)
         # return self.KLloss(p,q)
-        return tf.abs(tf.reduce_sum(p * (self.SpecialLog(p) - self.SpecialLog(q))))
+        # return tf.abs(tf.reduce_sum(p * (self.SpecialLog(p) - self.SpecialLog(q))))
 
     def KLloss(self, layerA, layerB):
         kl = (0.5 / self.num_nodes) * tf.reduce_mean(
