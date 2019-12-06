1bfe724aa1e6329aaf273ab801b5211587315eea
shanxuan <781244184@qq.com>
2019-12-06 13:05:55 +0800

    load data

--- HeterogeneousGraph/load_data.py
+++ HeterogeneousGraph/load_data.py
@@ -3,6 +3,7 @@ import numpy as np
 from os.path import join
 from utils import settings, string_utils
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
 
 enc = OneHotEncoder()
 IDF_THRESHOLD = 32  # small data
@@ -28,6 +29,12 @@ def constructAdj(pids):
     PAP = np.zeros(shape=( LenPids, LenPids ))
     PSP = np.zeros(shape=( LenPids, LenPids ))
     return PAP, PSP, pid2idx, idx2pid
+
+def constructIdx(X):
+    X_train, X_test = train_test_split(X, test_size=0.2, random_state=1)
+    X_train, X_val = train_test_split(X_train, test_size=0.2, random_state=1)
+    return X_train, X_val, X_test
+
 
 
 
@@ -62,18 +69,40 @@ def loadPSP(PSP, pid2idx, name, idf_threshold=IDF_THRESHOLD):
     return PSP
 
 
-def loadData(name, idf_threshold=32):
-    pass
-    # graph_dir = join(settings.DATA_DIR, 'local', 'graph-{}'.format(idf_threshold))
-    # PAP = open(join(graph_dir, '{}_PAP.txt'.format(name)), 'w')
-    # PSP = open(join(graph_dir, '{}_PSP.txt'.format(name)), 'w')
-    # feature = open(join(graph_dir, '{}_feature_and_label.txt'.format(name)), 'w')
-    # # build graph
-    # adj = buildGraph(join(path, "{}_pubs_network.txt".format(name)), idx_features_labels, features)
-    # adj2 = buildGraph(join(path, "{}_pubs_network2.txt".format(name)), idx_features_labels, features)
-    # idx_features_labels = np.genfromtxt(path, dtype=np.dtype(str))
-    #
-    # p
+def load_data_dblp(truelabels, truefeatures, PAP, PSP):
+    N = truefeatures.shape[0]
+    rownetworks = [PAP, PSP]
+
+    y = truelabels
+
+
+
+    train_idx = data['train_idx']
+    val_idx = data['val_idx']
+    test_idx = data['test_idx']
+
+    train_mask = sample_mask(train_idx, y.shape[0])
+    val_mask = sample_mask(val_idx, y.shape[0])
+    test_mask = sample_mask(test_idx, y.shape[0])
+
+    y_train = np.zeros(y.shape)
+    y_val = np.zeros(y.shape)
+    y_test = np.zeros(y.shape)
+    y_train[train_mask, :] = y[train_mask, :]
+    y_val[val_mask, :] = y[val_mask, :]
+    y_test[test_mask, :] = y[test_mask, :]
+
+    # return selected_idx, selected_idx_2
+    print('y_train:{}, y_val:{}, y_test:{}, train_idx:{}, val_idx:{}, test_idx:{}'.format(y_train.shape,
+                                                                                          y_val.shape,
+                                                                                          y_test.shape,
+                                                                                          train_idx.shape,
+                                                                                          val_idx.shape,
+                                                                                          test_idx.shape))
+    truefeatures_list = [truefeatures, truefeatures, truefeatures]
+    return rownetworks, truefeatures_list, y_train, y_val, y_test, train_mask, val_mask, test_mask
+
+
 
 
 if __name__ == '__main__':
@@ -90,6 +119,11 @@ if __name__ == '__main__':
     PSP = loadPSP(PSP, pid2idx, name)
     print (PSP)
     print (PSP.tolist())
+
+    N = len(pids)
+    X_train, X_val, X_test = constructIdx(list(range(N)))
+    print (X_train, X_val, X_test)
+
 
 
 
