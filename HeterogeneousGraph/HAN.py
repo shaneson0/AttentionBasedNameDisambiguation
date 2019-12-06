0fe350a4e88a35bbbb08bd57ed8cf5accf984ce3
shanxuan <781244184@qq.com>
2019-12-06 15:02:55 +0800

    load data

--- HeterogeneousGraph/HAN.py
+++ HeterogeneousGraph/HAN.py
@@ -28,13 +28,14 @@ class HAN():
         y_val[val_mask, :] = y[val_mask, :]
         y_test[test_mask, :] = y[test_mask, :]
 
-        # return selected_idx, selected_idx_2
-        print('y_train:{}, y_val:{}, y_test:{}, train_idx:{}, val_idx:{}, test_idx:{}'.format(y_train.shape,
-                                                                                              y_val.shape,
-                                                                                              y_test.shape,
-                                                                                              train_idx.shape,
-                                                                                              val_idx.shape,
-                                                                                              test_idx.shape))
+        # # return selected_idx, selected_idx_2
+        # print('y_train:{}, y_val:{}, y_test:{}, train_idx:{}, val_idx:{}, test_idx:{}'.format(y_train.shape,
+        #                                                                                       y_val.shape,
+        #                                                                                       y_test.shape,
+        #                                                                                       train_idx.shape,
+        #                                                                                       val_idx.shape,
+        #                                                                                       test_idx.shape))
         truefeatures_list = [truefeatures, truefeatures, truefeatures]
         return rownetworks, truefeatures_list, y_train, y_val, y_test, train_mask, val_mask, test_mask
+
 
