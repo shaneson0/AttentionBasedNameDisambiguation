a5d4b38753dd2dccbca1c6ff0a7611c68c62dc69
shanxuan <781244184@qq.com>
2019-12-07 23:29:42 +0800

    异构图

--- HeterogeneousGraph/HAN.py
+++ HeterogeneousGraph/HAN.py
@@ -112,13 +112,6 @@ class HAN():
         y_test[test_mask, :] = y[test_mask, :]
         y_all[all_mask, :] = y[all_mask, :]
 
-        # return selected_idx, selected_idx_2
-        print('y_train:{}, y_val:{}, y_test:{}, train_idx:{}, val_idx:{}, test_idx:{}'.format(y_train.shape,
-                                                                                              y_val.shape,
-                                                                                              y_test.shape,
-                                                                                              train_idx.shape,
-                                                                                              val_idx.shape,
-                                                                                              test_idx.shape))
         truefeatures_list = [truefeatures, truefeatures, truefeatures]
         return rownetworks, truefeatures_list, y_train, y_val, y_test, train_mask, val_mask, test_mask, y_all, all_mask
 
@@ -147,8 +140,6 @@ class HAN():
 
         adj_list, fea_list, y_train, y_val, y_test, train_mask, val_mask, test_mask, y_all, all_mask = self.load_data_dblp(labels,  features, PAP, PSP, X_train, X_val, X_test, Allidx)
         self.train(adj_list, fea_list, y_train, y_val, y_test, train_mask, val_mask, test_mask, y_all, all_mask)
-
-
 
     def sample_mask(self, idx, l):
         """Create mask."""
