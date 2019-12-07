62461b130ad6ea9ce62687d4f575c3b79fe8fb7b
shanxuan <781244184@qq.com>
2019-12-07 23:23:17 +0800

    异构图

--- HeterogeneousGraph/HAN.py
+++ HeterogeneousGraph/HAN.py
@@ -145,10 +145,7 @@ class HAN():
 
         #  truelabels, truefeatures, PAP, PSP, train_idx, val_idx, test_idx, allIdx
 
-        adj_list, fea_list, y_train, y_val, y_test, train_mask, val_mask, test_mask, y_all, all_mask = self.load_data_dblp(labels,
-                                                                                                         features, PAP,
-                                                                                                         PSP, X_train,
-                                                                                                         X_val, X_test, Allidx)
+        adj_list, fea_list, y_train, y_val, y_test, train_mask, val_mask, test_mask, y_all, all_mask = self.load_data_dblp(labels,  features, PAP, PSP, X_train, X_val, X_test, Allidx)
         self.train(adj_list, fea_list, y_train, y_val, y_test, train_mask, val_mask, test_mask, y_all, all_mask)
 
 
@@ -159,31 +156,6 @@ class HAN():
         mask[idx] = 1
         return np.array(mask, dtype=np.bool)
 
-    def load_data_dblp(self, truelabels, truefeatures, PAP, PSP, train_idx, val_idx, test_idx):
-        rownetworks = [PAP, PSP]
-
-        y = truelabels
-
-        train_mask = self.sample_mask(train_idx, y.shape[0])
-        val_mask = self.sample_mask(val_idx, y.shape[0])
-        test_mask = self.sample_mask(test_idx, y.shape[0])
-
-        y_train = np.zeros(y.shape)
-        y_val = np.zeros(y.shape)
-        y_test = np.zeros(y.shape)
-        y_train[train_mask, :] = y[train_mask, :]
-        y_val[val_mask, :] = y[val_mask, :]
-        y_test[test_mask, :] = y[test_mask, :]
-
-        # # return selected_idx, selected_idx_2
-        # print('y_train:{}, y_val:{}, y_test:{}, train_idx:{}, val_idx:{}, test_idx:{}'.format(y_train.shape,
-        #                                                                                       y_val.shape,
-        #                                                                                       y_test.shape,
-        #                                                                                       train_idx.shape,
-        #                                                                                       val_idx.shape,
-        #                                                                                       test_idx.shape))
-        truefeatures_list = [truefeatures, truefeatures, truefeatures]
-        return rownetworks, truefeatures_list, y_train, y_val, y_test, train_mask, val_mask, test_mask
 
     def getLabel(self, Y):
         Tlabels = self.enc.inverse_transform(Y)
