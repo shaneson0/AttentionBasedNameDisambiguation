c59ace6a72aee77f877cf7829e7b9a171021a492
shanxuan <781244184@qq.com>
2019-12-22 12:36:31 +0800

    check domain

--- models/MetrlcLoss.py
+++ models/MetrlcLoss.py
@@ -67,7 +67,7 @@ class OSM_CAA_Loss():
 
         L = (1 - self.l) * L_P + self.l * L_N
 
-        return L, denom
+        return L, tf.reduce_sum(denom, 1)
 
 if __name__ == '__main__':
     sess = tf.Session()
