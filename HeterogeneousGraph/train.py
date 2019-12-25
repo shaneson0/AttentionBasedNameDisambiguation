94b7626f5683c25343e1e0a7364e5133977486d2
shanxuan <781244184@qq.com>
2019-12-25 20:40:23 +0800

    train

--- HeterogeneousGraph/train.py
+++ HeterogeneousGraph/train.py
@@ -18,7 +18,8 @@ han = HAN()
 
 def testHAN(name):
     prec, rec, f1 = han.prepare_and_train(name=name)
-    print (prec, rec, f1)
+    return prec, rec, f1
+    # print (prec, rec, f1)
 
 def main():
     names = load_test_names()
@@ -51,33 +52,34 @@ def main():
 
 if __name__ == '__main__':
     # main()
-    testHAN(name)
-    # names = load_test_names()
-    # wf = codecs.open(join(settings.OUT_DIR, 'local_clustering_results.csv'), 'w', encoding='utf-8')
-    # wf.write('name,precision,recall,f1\n')
-    # macro_prec, macro_rec, macro_f1, count = 0.0, 0.0, 0.0, 0.0
-    # for name in names:
-    #
-    #     prec, rec, f1 = han.prepare_and_train(name=name)
-    #     print('name : ', name, 'prec, rec, f1: ', prec, rec, f1)
-    #     # cur_metric, num_nodes, n_clusters = train(name, True)
-    #     wf.write('{0},{1:.5f},{2:.5f},{3:.5f}\n'.format(
-    #         name, prec, rec, f1))
-    #     wf.flush()
-    #     macro_prec += prec
-    #     macro_rec += rec
-    #     macro_f1 += f1
-    #     count += 1
-    #
-    #     break
-    #
-    # macro_prec /= count
-    # macro_rec /= count
-    # macro_f1 /= count
-    #
-    # wf.write('average,,,{0:.5f},{1:.5f},{2:.5f}\n'.format(
-    #     macro_prec, macro_rec, macro_f1))
-    # wf.close()
+    # testHAN(name)
+    names = load_test_names()
+    wf = codecs.open(join(settings.OUT_DIR, 'local_clustering_results.csv'), 'w', encoding='utf-8')
+    wf.write('name,precision,recall,f1\n')
+    macro_prec, macro_rec, macro_f1, count = 0.0, 0.0, 0.0, 0.0
+    for name in names:
+
+        # prec, rec, f1 = han.prepare_and_train(name=name)
+        prec, rec, f1 = testHAN(name)
+        print('name : ', name, 'prec, rec, f1: ', prec, rec, f1)
+        # cur_metric, num_nodes, n_clusters = train(name, True)
+        wf.write('{0},{1:.5f},{2:.5f},{3:.5f}\n'.format(
+            name, prec, rec, f1))
+        wf.flush()
+        macro_prec += prec
+        macro_rec += rec
+        macro_f1 += f1
+        count += 1
+
+        break
+
+    macro_prec /= count
+    macro_rec /= count
+    macro_f1 /= count
+
+    wf.write('average,,,{0:.5f},{1:.5f},{2:.5f}\n'.format(
+        macro_prec, macro_rec, macro_f1))
+    wf.close()
 
 
 
