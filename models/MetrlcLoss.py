5820d49f224ff88faa1559ec3e4c1eca0e005339
shanxuan <781244184@qq.com>
2019-12-26 17:59:12 +0800

    test metric loss

--- models/MetrlcLoss.py
+++ models/MetrlcLoss.py
@@ -108,7 +108,7 @@ class OSM_CAA_Loss():
 
         L = (1 - self.l) * L_P + self.l * L_N
 
-        return L, [A]
+        return L, [L_P, L_N]
 
 if __name__ == '__main__':
     sess = tf.Session()
