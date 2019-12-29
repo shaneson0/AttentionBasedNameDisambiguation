3773093970d3cd9da416655e4ba562935d258400
shanxuan <781244184@qq.com>
2019-12-29 18:38:17 +0800

    fix nan

--- models/MetrlcLoss.py
+++ models/MetrlcLoss.py
@@ -58,8 +58,8 @@ class OSM_CAA_Loss():
         labels : (n,)
         embd : Fully Connected weights of classification layer (dxC), C is the number of classes: represents the vectors for class
         '''
-        # x = self.safe_divisor(x)
-        # x = tf.math.l2_normalize(x, 1)
+        x = self.safe_divisor(x)
+        x = tf.math.l2_normalize(x, 1)
         # n = self.n
         # r = tf.ones([n, 1], tf.float32)
         #
