e7ab89f283491c5252d89594febe5c3e97d8481c
shanxuan <781244184@qq.com>
2019-12-29 12:22:09 +0800

    break

--- HeterogeneousGraph/gen_train_data.py
+++ HeterogeneousGraph/gen_train_data.py
@@ -68,7 +68,7 @@ class DataGenerator:
         lc_emb = LMDBClient(raw_word2vec)
         LMDB_AUTHOR_FEATURE = "pub_authors.feature"
         lc_feature = LMDBClient(LMDB_AUTHOR_FEATURE)
-        cur_person_dict = self.names_train[authorName]
+        cur_person_dict = self.name2pubs_train[authorName]
         pids_set = set()
         pids = []
         pids2label = {}
