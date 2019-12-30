b021f2f0103e0ca019168daabf4a7dab533ec9b8
shanxuan <781244184@qq.com>
2019-12-30 11:17:10 +0800

    Triplete embedding

---
+++ global_/checkTrainDataEmbedding.py
@@ -0,0 +1,46 @@
+from utils.cache import LMDBClient
+from utils import data_utils, inputData, encode_labels, tSNEAnanlyse
+from utils import settings
+from global_.global_model import GlobalTripletModel
+import numpy as np
+from utils.eval_utils import get_hidden_output
+from os.path import abspath, dirname, join
+
+LMDB_NAME = "author_100.emb.weighted"
+lc_input = LMDBClient(LMDB_NAME)
+global_model = GlobalTripletModel(data_scale=1000000)
+trained_global_model = global_model.load_triplets_model()
+name_to_pubs_train = data_utils.load_json(settings.GLOBAL_DATA_DIR, 'name_to_pubs_train_500.json')
+
+names = ["gang_yin", "gang_zou", "guo_hua", "h_hu", "hai_yan_chen", "hai_yang_li"]
+
+for name in names:
+    name_data = name_to_pubs_train[name]
+
+    embs_input = []
+    pids = []
+    for i, aid in enumerate(name_data.keys()):
+        if len(name_data[aid]) < 5:  # n_pubs of current author is too small
+            continue
+        for pid in name_data[aid]:
+            cur_emb = lc_input.get(pid)
+            if cur_emb is None:
+                continue
+            embs_input.append(cur_emb)
+            pids.append(pid)
+
+    embedding = lc_input.get(name)
+    embs_input = np.stack(embs_input)
+    inter_embs = get_hidden_output(trained_global_model, embs_input)
+    labels = encode_labels(pids)
+
+    # Clustering and save the result
+    tSNEAnanlyse(inter_embs, labels, join(settings.PIC_DIR, "OnlyTriplete", "rawReature_%s_triplet.png" % (name)))
+    tSNEAnanlyse(embs_input, labels, join(settings.PIC_DIR, "OnlyTriplete", "rawReature_%s_features.png" % (name)))
+
+
+
+
+
+
+
