f5b58623bcbc802bfadb7639efe1ac06ce70bda4
shanxuan <781244184@qq.com>
2019-12-31 11:09:01 +0800

    check data length

--- HeterogeneousGraph/checklocalMetricLearning.py
+++ HeterogeneousGraph/checklocalMetricLearning.py
@@ -1,7 +1,17 @@
 
-from utils.cache import LMDBClient
+from utils.cache import LMDBClient, data_utils,settings
 
 LMDB_NAME_EMB = "lc_attention_network_embedding"
 lc_emb = LMDBClient(LMDB_NAME_EMB)
 
 print (lc_emb.getAllDataLength())
+
+name2pubs_train = data_utils.load_json(settings.GLOBAL_DATA_DIR, 'name_to_pubs_train_500.json')  # for test
+cnt = 0
+for name in name2pubs_train:
+    papers = name2pubs_train[name]
+    for aid in papers:
+        for pid in papers[aid]:
+            cnt += 1
+
+print ("all number of paper: ", cnt)
