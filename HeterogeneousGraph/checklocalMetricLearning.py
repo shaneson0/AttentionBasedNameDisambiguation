70e07d85001f41928bbf99cb4df11f27658f8c61
shanxuan <781244184@qq.com>
2019-12-31 11:10:56 +0800

    check data length

--- HeterogeneousGraph/checklocalMetricLearning.py
+++ HeterogeneousGraph/checklocalMetricLearning.py
@@ -8,10 +8,15 @@ print (lc_emb.getAllDataLength())
 
 name2pubs_train = data_utils.load_json(settings.GLOBAL_DATA_DIR, 'name_to_pubs_train_500.json')  # for test
 cnt = 0
+papers = []
 for name in name2pubs_train:
     papers = name2pubs_train[name]
     for aid in papers:
+        if len(papers[aid]) < 5:
+            continue
         for pid in papers[aid]:
-            cnt += 1
+            papers.append(pid)
 
-print ("all number of paper: ", cnt)
+
+
+print ("all number of paper: ", len(set(papers)))
