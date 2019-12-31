0cd053b6b792c4fa4efdb255af360af3cfdde8cb
shanxuan <781244184@qq.com>
2019-12-31 13:48:35 +0800

    train data set

--- global_/prepare_local_data.py
+++ global_/prepare_local_data.py
@@ -47,7 +47,27 @@ def dump_inter_emb():
             lc_inter.set(pid_, inter_embs[i])
             Res[pid_].append(inter_embs[i])
 
-    print(Res)
+    # the same as the train data
+    name_to_pubs_train = data_utils.load_json(settings.GLOBAL_DATA_DIR, 'name_to_pubs_train_500.json')
+    for name in name_to_pubs_train:
+        name_data = name_to_pubs_train[name]
+        embs_input = []
+        pids = []
+        for i, aid in enumerate(name_data.keys()):
+            if len(name_data[aid]) < 5:  # n_pubs of current author is too small
+                continue
+            for pid in name_data[aid]:
+                cur_emb = lc_input.get(pid)
+                if cur_emb is None:
+                    continue
+                embs_input.append(cur_emb)
+                pids.append(pid)
+        embs_input = np.stack(embs_input)
+        inter_embs = get_hidden_output(trained_global_model, embs_input)
+        for i, pid_ in enumerate(pids):
+            lc_inter.set(pid_, inter_embs[i])
+
+
 
 
 pubs_dict = data_utils.load_json(settings.GLOBAL_DATA_DIR, 'pubs_raw.json')
