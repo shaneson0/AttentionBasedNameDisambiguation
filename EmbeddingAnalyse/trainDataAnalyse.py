585513b4f3647efcd1395e1ee4ebe7dd251b4f90
shanxuan <781244184@qq.com>
2019-12-31 13:29:52 +0800

    check name

--- EmbeddingAnalyse/trainDataAnalyse.py
+++ EmbeddingAnalyse/trainDataAnalyse.py
@@ -18,30 +18,31 @@ def load_train_names():
 
 
 name_to_pubs_train= load_train_names()
-for name in name_to_pubs_train:
-    cur_author = name_to_pubs_train[name]
-    pids = []
-    labels = []
-    rf = []
-    tf = []
-    attentionf = []
+# for name in name_to_pubs_train:
+name = "yanjun_zhang"
+cur_author = name_to_pubs_train[name]
+pids = []
+labels = []
+rf = []
+tf = []
+attentionf = []
 
-    for aid in cur_author:
-        if len(cur_author[aid]) < 5:
-            continue
+for aid in cur_author:
+    if len(cur_author[aid]) < 5:
+        continue
 
-        for pid in cur_author[aid]:
-            pids.append(pid)
-            labels.append(aid)
-            rf.append(rawFeature.get(pid))
-            tf.append(tripletFeature.get(pid))
-            attentionf.append(lc_emb.get(pid))
+    for pid in cur_author[aid]:
+        pids.append(pid)
+        labels.append(aid)
+        rf.append(rawFeature.get(pid))
+        tf.append(tripletFeature.get(pid))
+        attentionf.append(lc_emb.get(pid))
 
-    labels = encode_labels(labels)
+labels = encode_labels(labels)
 
-    tSNEAnanlyse(rf, labels, join(settings.PIC_DIR, "FINALResult", "%s_rawFeature.png" % (name)))
-    tSNEAnanlyse(tf, labels, join(settings.PIC_DIR, "FINALResult", "%s_tripletFeature.png" % (name)))
-    tSNEAnanlyse(attentionf, labels, join(settings.PIC_DIR, "FINALResult", "%s_lcmbFeature.png" % (name)))
+tSNEAnanlyse(rf, labels, join(settings.PIC_DIR, "FINALResult", "%s_rawFeature.png" % (name)))
+tSNEAnanlyse(tf, labels, join(settings.PIC_DIR, "FINALResult", "%s_tripletFeature.png" % (name)))
+tSNEAnanlyse(attentionf, labels, join(settings.PIC_DIR, "FINALResult", "%s_lcmbFeature.png" % (name)))
 
 
 
