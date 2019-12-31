1ccc254e9ef7f111604e0479f5fe233cafc9078c
shanxuan <781244184@qq.com>
2019-12-31 12:42:15 +0800

    test

---
+++ EmbeddingAnalyse/trainDataAnalyse.py
@@ -0,0 +1,62 @@
+
+from os.path import abspath, dirname, join
+from utils.cache import LMDBClient
+from utils import data_utils, settings, encode_labels, tSNEAnanlyse
+
+rawFeatureLMDBName = "author_100.emb.weighted"
+rawFeature = LMDBClient(rawFeatureLMDBName)
+
+tripleteLossLMDBName = 'author_triplets.emb'
+tripletFeature = LMDBClient(tripleteLossLMDBName)
+
+LMDB_NAME_EMB = "lc_attention_network_embedding"
+lc_emb = LMDBClient(LMDB_NAME_EMB)
+
+def load_train_names():
+    name_to_pubs_train = data_utils.load_json(settings.GLOBAL_DATA_DIR, 'name_to_pubs_train_500.json')
+    return name_to_pubs_train
+
+
+name_to_pubs_train= load_train_names()
+for name in name_to_pubs_train:
+    cur_author = name_to_pubs_train[name]
+    pids = []
+    labels = []
+    rf = []
+    tf = []
+    attentionf = []
+
+    for aid in cur_author:
+        for pid in cur_author[aid]:
+            pids.append(pid)
+            labels.append(aid)
+            rf.append(rawFeature.get(pid))
+            tf.append(tripletFeature.get(pid))
+            attentionf.append(lc_emb.get(pid))
+
+    labels = encode_labels(labels)
+
+    tSNEAnanlyse(rf, labels, join(settings.PIC_DIR, "FINALResult", "%s_rawFeature.png" % (name)))
+    tSNEAnanlyse(tf, labels, join(settings.PIC_DIR, "FINALResult", "%s_tripletFeature.png" % (name)))
+    tSNEAnanlyse(attentionf, labels, join(settings.PIC_DIR, "FINALResult", "%s_lcmbFeature.png" % (name)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
