17ae4b8180adfd9647bb5660c66d57658dbe2e98
shanxuan <781244184@qq.com>
2020-01-01 23:45:43 +0800

    raw lc input

--- HeterogeneousGraph/prepare_local_data.py
+++ HeterogeneousGraph/prepare_local_data.py
@@ -19,7 +19,8 @@ def dump_inter_emb():
     dump hidden embedding via trained global_ model for local model to use
     """
     Res = defaultdict(list)
-    # LMDB_NAME = "author_100.emb.weighted"
+    LMDB_NAME = "author_100.emb.weighted"
+    raw_lc_input = LMDBClient(LMDB_NAME)
     LMDB_NAME = "lc_attention_network_embedding2"
     lc_input = LMDBClient(LMDB_NAME)
     INTER_LMDB_NAME = 'triplete_loss_lc_attention_network_embedding'
@@ -27,26 +28,26 @@ def dump_inter_emb():
     global_model = GlobalTripletModel(data_scale=1000000)
     trained_global_model = global_model.load_triplets_model()
 
-    # name_to_pubs_test = data_utils.load_json(settings.GLOBAL_DATA_DIR, 'name_to_pubs_test_100.json')
-    # # print(name_to_pubs_test)
-    # for name in name_to_pubs_test:
-    #     name_data = name_to_pubs_test[name]
-    #     embs_input = []
-    #     pids = []
-    #     for i, aid in enumerate(name_data.keys()):
-    #         if len(name_data[aid]) < 5:  # n_pubs of current author is too small
-    #             continue
-    #         for pid in name_data[aid]:
-    #             cur_emb = lc_input.get(pid)
-    #             if cur_emb is None:
-    #                 continue
-    #             embs_input.append(cur_emb)
-    #             pids.append(pid)
-    #     embs_input = np.stack(embs_input)
-    #     inter_embs = get_hidden_output(trained_global_model, embs_input)
-    #     for i, pid_ in enumerate(pids):
-    #         lc_inter.set(pid_, inter_embs[i])
-    #         Res[pid_].append(inter_embs[i])
+    name_to_pubs_test = data_utils.load_json(settings.GLOBAL_DATA_DIR, 'name_to_pubs_test_100.json')
+    # print(name_to_pubs_test)
+    for name in name_to_pubs_test:
+        name_data = name_to_pubs_test[name]
+        embs_input = []
+        pids = []
+        for i, aid in enumerate(name_data.keys()):
+            if len(name_data[aid]) < 5:  # n_pubs of current author is too small
+                continue
+            for pid in name_data[aid]:
+                cur_emb = raw_lc_input.get(pid)
+                if cur_emb is None:
+                    continue
+                embs_input.append(cur_emb)
+                pids.append(pid)
+        embs_input = np.stack(embs_input)
+        inter_embs = get_hidden_output(trained_global_model, embs_input)
+        for i, pid_ in enumerate(pids):
+            lc_inter.set(pid_, inter_embs[i])
+            Res[pid_].append(inter_embs[i])
 
     name_to_pubs_train = data_utils.load_json(settings.GLOBAL_DATA_DIR, 'name_to_pubs_train_500.json')
     for name in name_to_pubs_train:
