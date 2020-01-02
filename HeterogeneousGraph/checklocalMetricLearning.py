928ff291ce2f6929ec285112980b8a810fca210a
shanxuan <781244184@qq.com>
2020-01-02 11:40:34 +0800

    check Nonne

--- HeterogeneousGraph/checklocalMetricLearning.py
+++ HeterogeneousGraph/checklocalMetricLearning.py
@@ -5,8 +5,7 @@ from utils.cache import LMDBClient, data_utils,settings
 LMDB_NAME_EMB = "lc_attention_network_embedding2"
 lc_emb = LMDBClient(LMDB_NAME_EMB)
 
-print (lc_emb.getAllDataLength())
-
+NoneCnt = 0
 name2pubs_train = data_utils.load_json(settings.GLOBAL_DATA_DIR, 'name_to_pubs_train_500.json')  # for test
 cnt = 0
 cntpapers = []
@@ -17,23 +16,29 @@ for name in name2pubs_train:
             continue
         for pid in papers[aid]:
             cntpapers.append(pid)
+            emb = lc_emb.get(pid)
+            if emb is None:
+                NoneCnt += 1
+
+print ("None Cnt: ", NoneCnt)
 
 
 
-print ("all number of paper: ", len(set(cntpapers)))
 
-
-def testHAN():
-    han = HAN(lc_emb)
-    cntpapers = []
-
-    for name in name2pubs_train:
-        rawFeatures, labels, pids, rawlabels = han.loadFeature(name, ispretrain=True)
-        for pid in pids:
-            cntpapers.append(pid)
-
-    return len(set(cntpapers))
-
-print ("all number of train pids: ", testHAN())
+# print ("all number of paper: ", len(set(cntpapers)))
+#
+#
+# def testHAN():
+#     han = HAN(lc_emb)
+#     cntpapers = []
+#
+#     for name in name2pubs_train:
+#         rawFeatures, labels, pids, rawlabels = han.loadFeature(name, ispretrain=True)
+#         for pid in pids:
+#             cntpapers.append(pid)
+#
+#     return len(set(cntpapers))
+#
+# print ("all number of train pids: ", testHAN())
 
 
