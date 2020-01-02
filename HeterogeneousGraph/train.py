ae645cd430d4a162b016de7480baa62684eb022f
shanxuan <781244184@qq.com>
2020-01-02 09:08:10 +0800

    check triplete EMbedding

--- HeterogeneousGraph/train.py
+++ HeterogeneousGraph/train.py
@@ -15,17 +15,17 @@ def load_train_names():
     return name_to_pubs_train
 
 # name="kexin_xu"
-# name = "hongbin_li"
+name = "hongbin_li"
 # feiyu_kang
 # name = "feiyu_kang"
-name = "hai_yan_chen"
+# name = "hai_yan_chen"
 # din_ping_tsai
 
 def test(name):
     LMDB_NAME_EMB = "lc_attention_network_embedding"
     lc_emb = LMDBClient(LMDB_NAME_EMB)
     han = HAN(lc_emb)
-    prec, rec, f1 = han.prepare_and_train(name=name, ispretrain=True)
+    prec, rec, f1 = han.prepare_and_train(name=name, ispretrain=False, needtSNE=True)
     print (name, prec, rec, f1)
 
 def testHAN():
@@ -74,36 +74,8 @@ def testDataRun():
     wf.close()
 
 
-def main():
-    names = load_test_names()
-    wf = codecs.open(join(settings.OUT_DIR, 'local_clustering_results.csv'), 'w', encoding='utf-8')
-    wf.write('name,n_pubs,n_clusters,precision,recall,f1\n')
-    metrics = np.zeros(3)
-    cnt = 0
-    for name in names:
-        print('name : ', name)
-        cur_metric, num_nodes, n_clusters = train(name, True)
-        wf.write('{0},{1},{2},{3:.5f},{4:.5f},{5:.5f}\n'.format(
-            name, num_nodes, n_clusters, cur_metric[0], cur_metric[1], cur_metric[2]))
-        wf.flush()
-        for i, m in enumerate(cur_metric):
-            metrics[i] += m
-        cnt += 1
-        macro_prec = metrics[0] / cnt
-        macro_rec = metrics[1] / cnt
-        macro_f1 = eval_utils.cal_f1(macro_prec, macro_rec)
-        print('average until now', [macro_prec, macro_rec, macro_f1])
-        # time_acc = time.time()-start_time
-        # print(cnt, 'names', time_acc, 'avg time', time_acc/cnt)
-    macro_prec = metrics[0] / cnt
-    macro_rec = metrics[1] / cnt
-    macro_f1 = eval_utils.cal_f1(macro_prec, macro_rec)
-    wf.write('average,,,{0:.5f},{1:.5f},{2:.5f}\n'.format(
-        macro_prec, macro_rec, macro_f1))
-    wf.close()
-
 if __name__ == '__main__':
-    main()
+    test(name)
 
 
 
