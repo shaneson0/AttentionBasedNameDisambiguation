381b9fd9ea9167267a2baa71b6b58376ff35682f
shanxuan <781244184@qq.com>
2020-01-02 11:34:42 +0800

    raw2localTrans model

--- HeterogeneousGraph/raw2localTrans.py
+++ HeterogeneousGraph/raw2localTrans.py
@@ -53,7 +53,7 @@ def getRawEmbedding(pids):
 def getlocalTransEmbedding(pids):
     TransEmbedding = []
     for pid in pids:
-        TransEmbedding.append(lc_emb.get(pid))
+        TransEmbedding.append((lc_emb.get(pid)).reshape(1,-1))
     TransEmbedding = np.array(TransEmbedding)
     TransEmbedding.reshape(-1,1)
     return TransEmbedding
