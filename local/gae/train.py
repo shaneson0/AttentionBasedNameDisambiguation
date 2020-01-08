c17085de9baa2e6c59568f7c959911c1e1516081
shanxuan <781244184@qq.com>
2020-01-08 09:58:50 +0800

    change epochs

--- local/gae/train.py
+++ local/gae/train.py
@@ -32,7 +32,7 @@ from utils import settings, tSNEAnanlyse
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_float('learning_rate', 0.01, 'Initial learning rate.')
-flags.DEFINE_integer('epochs', 1000, 'Number of epochs to train.')
+flags.DEFINE_integer('epochs', 300, 'Number of epochs to train.')
 flags.DEFINE_integer('hidden1', 128, 'Number of units in hidden layer 1.')  # 32
 flags.DEFINE_integer('hidden2', 64, 'Number of units in hidden layer 2.')  # 16
 flags.DEFINE_float('weight_decay', 0., 'Weight for L2 loss on embedding matrix.')
