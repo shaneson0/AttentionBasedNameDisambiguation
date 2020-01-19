7282482993c2e7546038de016c760a5c9cca9ea9
shanxuan <781244184@qq.com>
2020-01-20 01:53:16 +0800

    各自训练metric space

--- HeterogeneousGraph/global_model.py
+++ HeterogeneousGraph/global_model.py
@@ -142,10 +142,16 @@ class GlobalTripletModel:
         encoded_emb_atten_pos = norm_layer(layer2_Atten(layer1_Atten(emb_atten_pos)))
         encoded_emb_atten_neg = norm_layer(layer2_Atten(layer1_Atten(emb_atten_neg)))
 
+
+
         Trans = Dense(64, activation=None, name='Anchor')
-        Anchor = Trans(K.concatenate([encoded_emb, encoded_emb_atten]))
-        Positive = Trans(K.concatenate[encoded_emb_pos, encoded_emb_atten_pos])
-        Negative = Trans(K.concatenate[encoded_emb_neg, encoded_emb_atten_neg])
+        T1 = K.concatenate([encoded_emb, encoded_emb_atten])
+        T2 = K.concatenate[encoded_emb_pos, encoded_emb_atten_pos]
+        T3 = K.concatenate[encoded_emb_neg, encoded_emb_atten_neg]
+
+        Anchor = Trans(T1)
+        Positive = Trans(T2)
+        Negative = Trans(T3)
 
         pos_dist = Lambda(euclidean_distance, name='pos_dist')([Anchor, Positive])
         neg_dist = Lambda(euclidean_distance, name='neg_dist')([Anchor, Negative])
